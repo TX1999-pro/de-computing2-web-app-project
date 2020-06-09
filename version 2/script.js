a8564a5e0d2c0c83461b275a5c44c20b8236f654
Tianxiao <tw1119@ic.ac.uk>
2020-06-09 17:52:38 +0800

    Enabled Timer and 'time's up' text

--- version 2/script.js
+++ version 2/script.js
@@ -9,25 +9,34 @@ const questionBank = [
         question: "What is an axolotl?",
         options:["A nerve in the brain", "A multi-axled vehicle",
         "A type of mortice lock", "A species of salamander"],
-        answer: 4
+        answer: 4,
+        description: "So what is salamander then :P"
     },
     {
         question: "The Panama Canal was officially "+
         "opened by which US president?",
         options:["Calvin Coolidge", "Herbert Hoover",
         "Theodore Roosevelt", "Woodrow Wilson"],
-        answer: 4
+        answer: 4,
+        description: "France began work on the canal in 1881, but stopped "+
+        "because of engineering problems and a high worker mortality rate. "+
+        "The United States took over the project in 1904 and opened the canal "+
+        "on August 15, 1914. "
     },
     {
         question: "What is a kudzu?",
         options:["Antelope", "Bird", "Jewish settlement", "Climbing plant"],
-        answer: 1
+        answer: 4,
+        description: "Today, kudzu is used to treat alcoholism and to reduce " +
+        "symptoms of alcohol hangover, including headache, " +
+        "upset stomach, dizziness, and vomiting. ..."
     },
     {
         question: "After Adam, Eve, Cain and Abel who is the next person " +
         "mentioned in the Bible?",
         options:["Enoch", "Jubal", "Lamech", "Zillah"],
-        answer: 1
+        answer: 1,
+        description:"He was of the Antediluvian period in the Hebrew Bible."
     }
 
 ];
@@ -37,10 +46,32 @@ const questionText = DQS(".question-text");
 const optionBox = DQS(".option-box");
 const answerDescription = DQS(".answer-description");
 const nextQuestionBtn = DQS(".next-question-btn");
-let questionIndex = 0;
+const seeResultBtn = DQS(".see-results-btn");
 const currentQuestionNum = DQS(".current-question-num");
 const correctAnswer = DQS(".correct-answers");
+const remainingTime = DQS(".remaining-time");
+const timeUpText = DQS(".time-up-text");
+
 let score=0;
+let questionIndex = 0;
+let interval = [];
+
+const hide = (el) => el.classList.remove("show");
+const show = (el) => el.classList.add("show");
+
+
+window.addEventListener("DOMContentLoaded", loadQuestion());
+nextQuestionBtn.addEventListener("click", nextQuestion);
+
+function loadQuestion() {
+    questionText.innerHTML = questionBank[questionIndex].question;
+    /* console.log(questionBank[questionIndex].question); */
+    createOptions();
+    timerStart();
+    scoreBoard();
+    currentQuestionNum.innerHTML = (questionIndex+1) + " / "
+     + questionBank.length;
+}
 
 function createOptions() {
     clearOptionBox();
@@ -56,9 +87,11 @@ function createOptions() {
 
 function clearOptionBox() {
     optionBox.innerHTML = "";
+    answerDescription.classList.remove("show");
 }
 
 function checkAnswer(element) {
+    timerStop ();
     const id= element.id;
     /* console.log("the answer is " + questionBank[questionIndex].answer); */
     if (id == questionBank[questionIndex].answer) {
@@ -68,10 +101,30 @@ function checkAnswer(element) {
     }
     else {
         element.classList.add("wrong");
+        // then show the right option
+        showCorrectAnswer();
     }
     disableSelection();
     showAnswerDescription();
-    showNextQuestionBtn();
+    questionIndex++;
+    if (questionIndex < questionBank.length) {
+        showNextQuestionBtn();
+    }
+    else {
+        quizOver();
+    }
+}
+
+function nextQuestion() {
+    if (questionIndex < questionBank.length) {
+        loadQuestion();
+        hide(answerDescription);
+        hide(nextQuestionBtn);
+        hide(timeUpText);
+    }
+    else {
+        quizOver();
+    }
 }
 
 function disableSelection() {
@@ -90,36 +143,69 @@ function disableSelection() {
 function showAnswerDescription() {
     const description = questionBank[questionIndex].description
     if (typeof description !== "undefined") {
-        answerDescription.classList.add("show");
+        show(answerDescription);
         answerDescription.innerHTML = description;
     }
 }
 
 function showNextQuestionBtn() {
-    nextQuestionBtn.classList.add("show");
+    show(nextQuestionBtn);
 }
 
 function scoreBoard() {
     correctAnswer.innerHTML = score;
 }
 
-nextQuestionBtn.addEventListener("click", nextQuestion);
 
-function nextQuestion() {
+function quizOver() {
+    hide(nextQuestionBtn);
+    show(seeResultBtn);
+    /* console.log("quiz over!") */
+
+}
+
+/* REMEBER: use callback to achieve timer function */
+function timerStart() {
+    let timeLimit = 10;
+    remainingTime.innerHTML = timeLimit;
+    remainingTime.classList.remove("less-time")
+    interval = setInterval(() => {
+        timeLimit--;
+        remainingTime.innerHTML = timeLimit;
+        if (timeLimit < 6 ) {
+            remainingTime.classList.add("less-time");
+        }
+        if (timeLimit == 0 ){
+                clearInterval(interval);
+                timeIsUp();
+        }
+    },1000);
+}
+
+function timerStop() {
+    clearInterval(interval);
+}
+
+function timeIsUp() {
+    // show time's up text when timer counts down to zero
+    timeUpText.classList.add("show")
+    // show correct answer when time is up
+    for(let i=0; i < optionBox.children.length; i++){
+        if(optionBox.children[i].id == questionBank[questionIndex].answer){
+            optionBox.children[i].classList.add("show-correct");
+        }
+    }
+    disableSelection();
+    showAnswerDescription();
+    showNextQuestionBtn();
+    showCorrectAnswer();
     questionIndex++
-    if (questionIndex+1 < questionBank.length) {
-        loadQuestion();
-    }
-    else {
-        console.log("this is the end of the quiz");
-    }
 }
-function loadQuestion() {
-    questionText.innerHTML = questionBank[questionIndex].question;
-    /* console.log(questionBank[questionIndex].question); */
-    createOptions();
-    scoreBoard();
-    currentQuestionNum.innerHTML = (questionIndex+1) + " / " + questionBank.length;
-}
-window.addEventListener("DOMContentLoaded", loadQuestion());
 
+function showCorrectAnswer(){
+    for(let i=0; i < optionBox.children.length; i++){
+        if(optionBox.children[i].id == questionBank[questionIndex].answer){
+            optionBox.children[i].classList.add("show-correct");
+        }
+    }
+}
